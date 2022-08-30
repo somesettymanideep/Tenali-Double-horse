74b3d6bffa9fbe53a3eea896c83b1f472286b612
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2022-08-30 08:10:02 -0700

    footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -28,44 +28,44 @@ class Footer extends Component {
                                 <h5 className="widget-title">Quick Links</h5>
                                 <ul>
                                     <li> <Link to="/">Gallery</Link> </li>
-                                    <li> <Link to="/blog-grid">Products</Link> </li>
-                                    <li> <Link to="/about">Recipes</Link> </li>
-                                    <li> <Link to="/menu-v1">Blog</Link> </li>
-                                    <li> <Link to="/contact">Contact</Link> </li>
-                                    <li> <Link to="/contact">Shop</Link> </li>
-                                    <li> <Link to="/contact">Usa</Link> </li>
+                                    <li> <Link to=" ">Products</Link> </li>
+                                    <li> <Link to=" ">Recipes</Link> </li>
+                                    <li> <Link to=" ">Blog</Link> </li>
+                                    <li> <Link to=" ">Contact</Link> </li>
+                                    <li> <Link to=" ">Shop</Link> </li>
+                                    <li> <Link to=" ">Usa</Link> </li>
                                 </ul>
                             </div>
                             <div className="col-xl-3 col-lg-3 col-md-4 col-sm-12 footer-widget">
                                 <h5 className="widget-title">Legal Links</h5>
                                 <ul>
-                                    <li> <Link to="/menu-v1">Terms of Use</Link> </li>
-                                    <li> <Link to="/menu-v1">Privacy Policy</Link> </li>
-                                    <li> <Link to="/menu-v1">Shipping Policy</Link> </li>
-                                    <li> <Link to="/menu-v1">Refund Policy</Link> </li>
-                                    <li> <Link to="/menu-v1">Payment Services</Link> </li>
+                                    <li> <Link to=" ">Terms of Use</Link> </li>
+                                    <li> <Link to=" ">Privacy Policy</Link> </li>
+                                    <li> <Link to=" ">Shipping Policy</Link> </li>
+                                    <li> <Link to=" ">Refund Policy</Link> </li>
+                                    <li> <Link to=" ">Payment Services</Link> </li>
                                 </ul>
                             </div>
                             <div className="col-xl-3 col-lg-3 col-md-4 col-sm-12 footer-widget">
                                 <h5 className="widget-title">Contact</h5>
                                 <ul>
-                                    <li> <Link to="/checkout">Call Us</Link> </li>
-                                    <li> <Link to="/cart">Email</Link> </li>
-                                    <li> <Link to="/menu-item-v1">Locate</Link> </li>
+                                    <li> <Link to=" ">Call Us</Link> </li>
+                                    <li> <Link to=" ">Email</Link> </li>
+                                    <li> <Link to=" ">Locate</Link> </li>
                                     
                                 </ul>
                             </div>
                             <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12 footer-widget">
                                 <h5 className="widget-title">Social Media</h5>
                                 <ul className="social-media">
-                                    <li> <Link to="#" className="facebook"> <i className="fab fa-facebook-f" /> </Link> </li>
-                                    <li> <Link to="#" className="pinterest"> <i className="fab fa-pinterest-p" /> </Link> </li>
-                                    <li> <Link to="#" className="google"> <i className="fab fa-google" /> </Link> </li>
-                                    <li> <Link to="#" className="twitter"> <i className="fab fa-twitter" /> </Link> </li>
+                                    <li> <Link to=" " className="facebook"> <i className="fab fa-facebook-f" /> </Link> </li>
+                                    <li> <Link to=" " className="pinterest"> <i className="fab fa-pinterest-p" /> </Link> </li>
+                                    <li> <Link to=" " className="google"> <i className="fab fa-google" /> </Link> </li>
+                                    <li> <Link to="" className="twitter"> <i className="fab fa-twitter" /> </Link> </li>
                                 </ul>
                                 <div className="footer-offer">
                                     <p>Signup and get exclusive offers and coupon codes</p>
-                                    <Link to="/register" className="btn-custom btn-sm shadow-none">Sign Up</Link>
+                                    <Link to=" " className="btn-custom btn-sm shadow-none">Sign Up</Link>
                                 </div>
                             </div>
                         </div>
@@ -75,14 +75,14 @@ class Footer extends Component {
                 <div className="footer-bottom">
                     <div className="container">
                         <ul>
-                            <li> <Link to="#">Privacy Policy</Link> </li>
-                            <li> <Link to="#">Refund Policy</Link> </li>
-                            <li> <Link to="#">Cookie Policy</Link> </li>
-                            <li> <Link to="#">Terms &amp; Conditions</Link> </li>
+                            <li> <Link to="">Privacy Policy</Link> </li>
+                            <li> <Link to="">Refund Policy</Link> </li>
+                            <li> <Link to="">Cookie Policy</Link> </li>
+                            <li> <Link to="">Terms &amp; Conditions</Link> </li>
                         </ul>
                         <div className="footer-copyright">
-                            <p> Copyright © 2022 <Link to="#">Tenali Double Horse </Link> All Rights Reserved. </p>
-                            <Link to="#" className="back-to-top" onClick={() => this.scrollToTop()}>Back to top <i className="fas fa-chevron-up" /> </Link>
+                            <p> Copyright © 2022 <Link to=" ">Tenali Double Horse </Link> All Rights Reserved. </p>
+                            <Link to=" " className="back-to-top" onClick={() => this.scrollToTop()}>Back to top <i className="fas fa-chevron-up" /> </Link>
                         </div>
                     </div>
                 </div>

c921db7e8af28e820242c4b6590a16253da1ab94
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2022-08-29 21:42:09 -0700

    first commit

---
+++ src/components/sections/home-v2/Content.js
@@ -0,0 +1,39 @@
+import React, { Component, Fragment } from 'react';
+import Products from '../home/Products';
+import Testimonials from '../home/Testimonials';
+import Banner from './Banner';
+import Categories from './Categories';
+import Newsletter from './Newsletter';
+import Offer from './Offer';
+
+
+class Content extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Banner />
+                <div className='activites'>
+                    <h2 className='title text-center'>Notable Activities</h2>
+                    <div className='social-media'>
+                        <img src={process.env.PUBLIC_URL + "/assets/img/misc/aha.png"} className="aha" alt="aha"/>
+                        <img src={process.env.PUBLIC_URL + "/assets/img/misc/amma-kuchi.png"} className="aha" alt="aha"/>
+                        <img src={process.env.PUBLIC_URL + "/assets/img/misc/double-horse.png"} className="aha" alt="aha"/>
+                    </div>
+                    
+
+                </div>
+                
+                <Offer />
+                <Categories />
+                
+                <div className="section pt-0 products-section">
+                    <Products />
+                </div>
+                
+                <Newsletter />
+            </Fragment>
+        );
+    }
+}
+
+export default Content;
---
+++ src/components/sections/home-v2/Offer.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Offer extends Component {
+    render() {
+        return (
+            <div className="section light-bg">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6 mb-lg-30">
+                            <img src={process.env.PUBLIC_URL + "/assets/img/misc/Eco Pack.jpg"} alt="img" />
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="section-title-wrap mr-lg-30">
+                                <h5 className="custom-primary">Special Edition</h5>
+                                <h2 className="title">Why Choose Tenali Double Horse Eco Pack?</h2>
+                                <p className="subtitle">
+                                In a world where almost everything adds to pollution or negatively influences nature and human health, Tenali Double Horse has come up with using eco-friendly products, to break this chain.
+          </p>
+                                <p className="subtitle">
+                                We encourage you to switch to the right choices, as it is the need of the hour!
+          </p>
+                                <Link to="/menu-item-v1" className="btn-custom">Order Now</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Offer;

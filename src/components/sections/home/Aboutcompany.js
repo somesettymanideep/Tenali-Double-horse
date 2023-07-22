6dc447de536baf29da307119d6dd4d3b0277f21c
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2023-07-21 23:53:47 -0700

    about changes done

--- src/components/sections/home/Aboutcompany.js
+++ src/components/sections/home/Aboutcompany.js
@@ -46,7 +46,8 @@ class Aboutcompany extends Component {
                                 <li className="breadcrumb-item "><h5 className="custom-primary" style={{marginBottom:'10px'}}>About</h5></li>
                                 <li className="breadcrumb-item "><h2 className="title" style={{marginBottom:'10px'}}>Tenali Double Horse</h2></li>
                                 <li className="breadcrumb-item active text-dark" aria-current="page">Since launching in 2005,  Tenali Double Horse has empowered many lives in India and gained millions of people. By putting all the pieces of a successful business in one convenient platform, Tenali Double Horse’s impeccable distribution network got started and grew faster. We provide the best quality Urad Gota and Urad Dall near you.</li>
-                                <a href={"/about"} className="btn-custom  mt-4">Read more</a>
+                                {/* <a href={"/about"} className="btn-custom  mt-4">Read more</a> */}
+                                <Link  to={"/about"} className="btn-custom primary mt-4">Read More  </Link>
                             </ol>
                         </nav>
                     </div>

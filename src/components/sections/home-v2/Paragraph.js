430c9dff037c7b863da8b504f9513005ce94b8a0
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2023-08-02 22:55:27 -0700

    pdf upadted

--- src/components/sections/home-v2/Paragraph.js
+++ src/components/sections/home-v2/Paragraph.js
@@ -3,6 +3,7 @@ import { Modal } from 'react-bootstrap';
 // import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+
 class Paragraph extends Component {
   constructor(props) {
     super(props);
@@ -20,21 +21,38 @@ class Paragraph extends Component {
   }
 
 
+  // handleSaveChanges = () => {
+  //   const content = 'Hello, this is a PDF!'; // Replace this with the content you want in the PDF
+
+  //   // Convert the content to a Blob
+  //   const pdfBlob = new Blob([content], { type: 'application/pdf' });
+
+  //   // Create a download link
+  //   const downloadLink = document.createElement('a');
+  //   downloadLink.href = URL.createObjectURL(pdfBlob);
+  //   downloadLink.download = 'my_document.pdf';
+  //   downloadLink.click();
+
+  //   // Close the modal after saving
+  //   this.handleClose();
+  // };
   handleSaveChanges = () => {
-    const content = 'Hello, this is a PDF!'; // Replace this with the content you want in the PDF
-
-    // Convert the content to a Blob
-    const pdfBlob = new Blob([content], { type: 'application/pdf' });
-
-    // Create a download link
-    const downloadLink = document.createElement('a');
-    downloadLink.href = URL.createObjectURL(pdfBlob);
-    downloadLink.download = 'my_document.pdf';
-    downloadLink.click();
-
-    // Close the modal after saving
-    this.handleClose();
+    try {
+      // Replace './tdh-brouchure.pdf' with the correct URL to your PDF file
+      const pdfUrl = process.env.PUBLIC_URL + "/assets/img/people/tdh-brouchure.pdf";
+  
+      // Open the PDF file URL in a new tab
+      window.open(pdfUrl, '_blank');
+  
+      // Close the modal after saving
+      this.handleClose();
+    } catch (error) {
+      console.error('Error opening PDF:', error);
+      // Handle error if needed
+    }
   };
+  
+  
 
   render() {
     const { show } = this.state;

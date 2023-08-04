476bf26607d3470cc6e804391d3ae2e1265e9ca9
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2023-08-04 00:56:30 -0700

    approval changes

--- src/components/sections/blog-grid/Content.js
+++ src/components/sections/blog-grid/Content.js
@@ -3,11 +3,13 @@ import React, { Component , Fragment} from 'react';
 import Mapcountries from './Mapcountries';
 import Newsletter from '../home-v2/Newsletter';
 import Cards from './Productcards';
+import Mapointing from './Mapointing';
 
 class Content extends Component {
     render() {
         return (
             <Fragment>
+                <Mapointing/>
      <Mapcountries/>
             
                 {/* <Cards/> */}

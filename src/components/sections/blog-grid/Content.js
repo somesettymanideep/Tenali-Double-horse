b279f493f0e1ebf013481c1c66bee09c373b4ead
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2023-08-24 00:56:18 -0700

    markers

--- src/components/sections/blog-grid/Content.js
+++ src/components/sections/blog-grid/Content.js
@@ -1,9 +1,49 @@
 import React, { Component , Fragment} from 'react';
 
 
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { ComposableMap, Geographies, Geography ,Marker} from "react-simple-maps";
 
 const geoUrl ="https://cdn.jsdelivr.net/npm/world-atlas@2/land-110m.json"
+
+const markers = [
+    {
+      markerOffset: -30,
+      name: "India",
+      coordinates: [78.9629, 20.5937], // India coordinates [longitude, latitude]
+    },
+    {
+      markerOffset: 15,
+      name: "Australia",
+      coordinates: [133.7751, -25.2744], // Australia coordinates [longitude, latitude]
+    },
+    {
+      markerOffset: 15,
+      name: "Europe",
+      coordinates: [15.2551, 54.5260], // Europe coordinates [longitude, latitude] (Note: This is just an approximate central location of Europe)
+    },
+    {
+      markerOffset: 15,
+      name: "USA",
+      coordinates: [-95.7129, 37.0902], // USA coordinates [longitude, latitude]
+    },
+    {
+      markerOffset: 15,
+      name: "UK",
+      coordinates: [-3.4359, 55.3781], // UK coordinates [longitude, latitude]
+    },
+    {
+      markerOffset: 15,
+      name: "South Africa",
+      coordinates: [24.0232, -30.5595], // South Africa coordinates [longitude, latitude]
+    },
+    {
+      markerOffset: 15,
+      name: "UAE",
+      coordinates: [53.8478, 23.4241], // UAE coordinates [longitude, latitude]
+    },
+    // Add more markers if needed...
+  ];
+  
 
 
 class Content extends Component {
@@ -15,10 +55,39 @@ class Content extends Component {
       <Geographies geography={geoUrl}>
         {({ geographies }) =>
           geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
+            <Geography key={geo.rsmKey} geography={geo} fill="#EAEAEC"
+                                stroke="#D6D6DA" />
           ))
         }
       </Geographies>
+      {markers.map(({ name, coordinates, markerOffset }) => (
+              <Marker key={name} coordinates={coordinates}>
+                <g>
+                  
+                    <>
+                      <g
+                        fill="none"
+                        stroke="#FF5533"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        transform="translate(-12, -24)"
+                      >
+                        <circle cx="12" cy="10" r="3" />
+                        <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                      </g>
+                      <text
+                        textAnchor="middle"
+                        y={markerOffset}
+                        style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+                      >
+                        {name}
+                      </text>
+                    </>
+                  
+                </g>
+              </Marker>
+            ))}
     </ComposableMap>
   </div>
                 
